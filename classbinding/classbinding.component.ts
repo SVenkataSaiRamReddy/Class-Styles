95a1fbc6bb17acff27cb8c20526603f3edd17c7e
Ram <svsramreddy92@gmail.com>
2024-02-25 12:57:46 +0530

    Class and Styles

---
+++ classbinding/classbinding.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-classbinding',
+  templateUrl: './classbinding.component.html',
+  styleUrl: './classbinding.component.css'
+})
+export class ClassbindingComponent {
+
+  addhighlight : boolean = false;
+}
